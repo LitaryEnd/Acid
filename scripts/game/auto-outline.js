52fee0256b84b1e0671a04fb14f3dcffba7d977d
Toxin <82654581+LitaryEnd@users.noreply.github.com>
2021-10-29 16:55:25 +0300

    Add files via upload
    0.66.5

---
+++ scripts/game/auto-outline.js
@@ -0,0 +1,4 @@
+      const vec1 = new Vec2();
+		const vec2 = new Vec2();
+		const tPos = new Vec2();
+
---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+require ("game/acid-copters") //fixed
+
+//require ("game/auto-outline") 
+require ("game/funclib") 
+
+//require ("ui/acid-logo")
+//require ("game-planet/acid-sectors")
+
+//require ("game/acid-null") 
+//require ("game/acid-null") 
